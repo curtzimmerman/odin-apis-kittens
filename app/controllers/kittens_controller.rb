d03e1cbc7ba43e385f7613d97ee5308ef8baea60
Curt Zimmerman <czimmerman9@gmail.com>
2015-06-11 19:24:19 -0400

    created kittens resource

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,51 @@
+class KittensController < ApplicationController
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "kitten created"
+      redirect_to kittens_path
+    else
+      flash.now[:error] = "error"
+      render 'new'
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id]) 
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    @kitten.update_attributes(kitten_params)
+    if @kitten.save
+      flash[:success] = "kitten updated"
+      redirect_to kittens_path
+    else
+      flash.now[:error] = "error"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Kitten.find(params[:id]).destroy
+    redirect_to kittens_path
+  end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+end
